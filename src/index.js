36cd569f5b7afaa307bda64be4f3126d81fbd9f2
Dinar <163840209+redinar@users.noreply.github.com>
2024-07-31 15:12:04 +0300

    add soultion for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let i = 0;
+  let result = '';
+  str = n.toString();
+
+  if (str[0] === '-'){
+      str = str.slice(1);
+  } 
+
+  while (i < str.length) { 
+    result = str[i] + result;
+    i++;
+  }
+  return Number(result);
 }
